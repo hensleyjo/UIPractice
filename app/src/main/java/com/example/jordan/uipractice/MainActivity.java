f0661bd9562f3ea9ef0f18246be59e09537fb89c
hensleyjo <hensleyjo@gmail.com>
2016-05-16 13:04:17 -0600

    Got drag part working

--- app/src/main/java/com/example/jordan/uipractice/MainActivity.java
+++ app/src/main/java/com/example/jordan/uipractice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jordan.uipractice;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Assign the touch listener to your view which you want to move
+        findViewById(R.id.myImage).setOnTouchListener(new MyTouchListener());
     }
+
+    // This defines your touch listener
+    private final class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
 }
